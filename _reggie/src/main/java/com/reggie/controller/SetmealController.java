a4fea3e675604a0728abe152d606d8c1d2938b7c
tanhuajia <tanhuajia@apical.com.cn>
2022-12-06 17:55:20 +0800

    Revert "v1.2"
    This reverts commit ea8b94f9cfd28bdca5d53bf5c03185f404b09693.

--- _reggie/src/main/java/com/reggie/controller/SetmealController.java
+++ _reggie/src/main/java/com/reggie/controller/SetmealController.java
@@ -2,20 +2,12 @@ package com.reggie.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.reggie.dto.SetmealDto;
-import com.reggie.entity.Category;
 import com.reggie.entity.Setmeal;
-import com.reggie.service.CategoryService;
 import com.reggie.service.SetmealService;
 import com.reggie.util.R;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -24,80 +16,33 @@ public class SetmealController {
     @Autowired
     private SetmealService setmealService;
 
-    @Autowired
-    private CategoryService categoryService;
-
     @GetMapping("/page")
-    public R<Page> page(int page, int pageSize,String name){
+    public R page(int page, int pageSize){
         Page<Setmeal> pageInfo = new Page<>(page,pageSize);
-        Page<SetmealDto> pageDtoInfo = new Page<>();
         LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.like(!StringUtils.isEmpty(name),Setmeal::getName,name);
-        queryWrapper.orderByDesc(Setmeal::getUpdateTime);
         setmealService.page(pageInfo,queryWrapper);
-        BeanUtils.copyProperties(pageInfo,pageDtoInfo,"records");
-        List<Setmeal> records = pageInfo.getRecords();
-        List<SetmealDto> list = records.stream().map((item) -> {
-            SetmealDto setmealDto = new SetmealDto();
-            BeanUtils.copyProperties(item,setmealDto);
-            Long categoryId = item.getCategoryId();
-            Category category = categoryService.getById(categoryId);
-            if (category!=null){
-                String categoryName = category.getName();
-                setmealDto.setCategoryName(categoryName);
-            }
-            return setmealDto;
-        }).collect(Collectors.toList());
-        pageDtoInfo.setRecords(list);
-        return R.success(pageDtoInfo);
+        return R.success(pageInfo);
     }
 
     @PostMapping
-    public R save(@RequestBody SetmealDto setmealDto){
-        log.info("添加套餐：{}",setmealDto);
-        setmealService.saveWithDish(setmealDto);
+    public R save(@RequestBody Setmeal setmeal){
+        log.info("添加套餐：{}",setmeal);
+        setmealService.save(setmeal);
         return R.success("添加套餐成功");
     }
 
     @DeleteMapping
-    public R delete(String[] ids){
-        int index = 0;
-        for (String id : ids) {
-            Setmeal setmeal = setmealService.getById(id);
-            if (setmeal.getStatus() != 1){
-                setmealService.removeById(id);
-            }else{
-                index++;
-            }
-        }
-        if(index > 0 && index == ids.length){
-            return R.error("选中的套餐均为启售状态，不能删除");
-        }else{
-            return R.success("删除成功");
-        }
-    }
-
-    @PostMapping("/status/{status}")
-    public R<String> sale(@PathVariable int status,String[] ids){
-        for (String id : ids) {
-            Setmeal setmeal = setmealService.getById(id);
-            setmeal.setStatus(status);
-            setmealService.updateById(setmeal);
-        }
-        return R.success("修改成功");
+    public R delete(Long id){
+        log.info("要删除的套餐：{}",id);
+        setmealService.removeById(id);
+        return R.success("删除套餐成功");
     }
 
     @PutMapping
-    public R<String> update(@RequestBody SetmealDto setmealDto){
-        setmealService.updateWithDish(setmealDto);
-        return R.success("修改成功");
-    }
-
-    //根据id查询套餐信息
-    @GetMapping("/{id}")
-    public R<SetmealDto> getById(@PathVariable Long id){
-        SetmealDto setmealDto = setmealService.getByIdWithDish(id);
-        return R.success(setmealDto);
+    public R update(@RequestBody Setmeal setmeal){
+        log.info("更新套餐：{}",setmeal);
+        setmealService.updateById(setmeal);
+        return R.success("更新套餐成功");
     }
 
 }
--- _reggie/src/main/java/com/reggie/service/impl/SetmealServiceImpl.java
+++ _reggie/src/main/java/com/reggie/service/impl/SetmealServiceImpl.java
@@ -1,65 +1,11 @@
 package com.reggie.service.impl;
 
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.reggie.dto.SetmealDto;
 import com.reggie.entity.Setmeal;
-import com.reggie.entity.SetmealDish;
 import com.reggie.mapper.SetmealMapper;
-import com.reggie.service.SetmealDishService;
 import com.reggie.service.SetmealService;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class SetmealServiceImpl extends ServiceImpl<SetmealMapper, Setmeal> implements SetmealService {
-    @Autowired
-    private SetmealDishService setmealDishService;
-
-    @Override
-    @Transactional
-    public void saveWithDish(SetmealDto setmealDto) {
-        //保存套餐基本信息，操作setmeal,执行insert操作
-        this.save(setmealDto);
-        List<SetmealDish> setmealDishes = setmealDto.getSetmealDishes();
-        setmealDishes.stream().map((item) -> {
-            item.setSetmealId(setmealDto.getId());
-            return item;
-        }).collect(Collectors.toList());
-        //保存套餐和菜品的关联信息,操作setmeal_dish,执行insert操作
-        setmealDishService.saveBatch(setmealDishes);
-    }
-
-    @Override
-    public SetmealDto getByIdWithDish(Long id) {
-        Setmeal setmeal = this.getById(id);
-        SetmealDto setmealDto = new SetmealDto();
-        BeanUtils.copyProperties(setmeal,setmealDto);
-        //查询套餐菜品信息
-        LambdaQueryWrapper<SetmealDish> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(SetmealDish::getSetmealId,setmeal.getId());
-        List<SetmealDish> list = setmealDishService.list(queryWrapper);
-        setmealDto.setSetmealDishes(list);
-        return setmealDto;
-    }
-
-    @Override
-    public void updateWithDish(SetmealDto setmealDto) {
-        this.updateById(setmealDto);
-        LambdaQueryWrapper<SetmealDish> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(SetmealDish::getSetmealId,setmealDto.getId());
-        setmealDishService.remove(queryWrapper);
-
-        List<SetmealDish> setmealDishes = setmealDto.getSetmealDishes();
-        setmealDishes = setmealDishes.stream().map((item) -> {
-            item.setSetmealId(setmealDto.getId());
-            return item;
-        }).collect(Collectors.toList());
-        setmealDishService.saveBatch(setmealDishes);
-    }
 }
